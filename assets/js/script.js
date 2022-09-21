7a35b9f1f723cc7d3ff013b8679cb36d78054dc3
Pavle <105302576+PavleMarinkovic95@users.noreply.github.com>
2022-09-21 16:22:59 +0200

    Fixed comments and separation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Adding constants and logic
+// Adding constants and logic.
 const game = () => {
   let playerScore = 0;
   let computerScore = 0;
@@ -8,7 +8,7 @@ const game = () => {
   let playerC;
 
 
-  // connetcts the options with the buttons also loads them to player options and comp options
+  // Connects the options with the buttons, also loads them to player options and computer options.
   const playGame = () => {
     const rockBtn = document.querySelector('.rock');
     const paperBtn = document.querySelector('.paper');
@@ -18,7 +18,7 @@ const game = () => {
     const playerOptions = [rockBtn,paperBtn,scissorBtn, spokBtn,lizardrBtn];
     const computerOptions = ['rock','paper','scissors','spock','lizard'];
     
-    // Function to start playing and, random computor chice form 0 to 4 wich is 5 choices :)
+    // Function to start playing and, random computer choice form 0 to 4 wich is 5 choices.
     // Also to show each choice on each move 
     playerOptions.forEach(option => {
       option.addEventListener('click',function(){
@@ -26,12 +26,12 @@ const game = () => {
         playerC = option.textContent;
         playerText.textContent = `Player: ${playerC}`;
 
-      // increments the moves so that there are 15 in total
+      // Increments the moves so that there are 15 in total
         const movesLeft = document.querySelector('.movesleft');
         moves++;
         movesLeft.innerText = `Moves Left: ${15-moves}`;
         
-      // this gives a random number 
+      // This gives a random number to the computer and floors it to make a choice
         const choiceNumber = Math.floor(Math.random()*5);
         const computerChoice = computerOptions[choiceNumber];
         
@@ -56,6 +56,9 @@ const game = () => {
     const computerScoreBoard = document.querySelector('.computer-count');
     player = player.toLowerCase();
     computer = computer.toLowerCase();
+
+  // Checks the rock option
+
     if(player === computer){
       result.textContent = 'Draw!';
     }
@@ -71,6 +74,9 @@ const game = () => {
         playerScoreBoard.textContent = playerScore;
       }
     }
+
+  // Checks the scissors option
+
     else if(player == 'scissors'){
       if(computer == 'rock' || computer == 'spock'){
         result.textContent = 'Computer wins!';
@@ -82,6 +88,9 @@ const game = () => {
         playerScoreBoard.textContent = playerScore;
       }
     }
+
+  // Checks the paper option
+
     else if(player == 'paper'){
       if(computer == 'scissors' || computer == 'lizard'){
         result.textContent = 'Computer wins!';
@@ -93,6 +102,9 @@ const game = () => {
         playerScoreBoard.textContent = playerScore;
       }
     }
+
+  // Checks the spock option
+
     else if(player == 'spock'){
       if(computer == 'paper' || computer == 'lizard'){
         result.textContent = 'Computer wins!';
@@ -104,6 +116,9 @@ const game = () => {
         playerScoreBoard.textContent = playerScore;
       }
     }
+
+  // Checks the lizard option
+
     else if(player == 'lizard'){
       if(computer == 'scissors' || computer == 'rock'){
         result.textContent = 'Computer wins!';
@@ -117,7 +132,7 @@ const game = () => {
     }
   };
 
-  // Function to run when game is over
+  // Function to run when game is over and hide options 
   const gameOver = (playerOptions,movesLeft) => {
 
     const chooseMove = document.querySelector('.move');
@@ -128,25 +143,29 @@ const game = () => {
       option.style.display = 'none';
     });
 
-    // Declearing the winner 
+    // Declearing the winner and also adding some style to the declaration
     chooseMove.innerText = 'Game Over!!';
     movesLeft.style.display = 'none';
 
+    // In case the player won
     if(playerScore > computerScore){
       result.style.fontSize = '2rem';
       result.innerText = 'You Won The Game';
       result.style.color = '#308D46';
     }
+    // In case the Computer won
     else if(playerScore < computerScore){
       result.style.fontSize = '2rem';
       result.innerText = 'You Lost The Game';
       result.style.color = 'red';
     }
+    // In case the game is a draw 
     else{
       result.style.fontSize = '2rem';
       result.innerText = 'Its a Draw!';
       result.style.color = 'grey';
     }
+    // Calling the Restard button 
     reloadBtn.innerText = 'Restart';
     reloadBtn.style.display = 'flex';
     reloadBtn.addEventListener('click',() => {
